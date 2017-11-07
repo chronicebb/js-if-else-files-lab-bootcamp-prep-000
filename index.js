cc32c5eaa41f94d81d19270dd1474aae2ee2abb4
Jonathan Sawyer <jojosawyer98@gmail.com>
2017-11-07 16:18:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-var name = "Joe"
+var name = "Susan"
 var height = "74"
 
 
 // Don't worry about this
 module.exports = { name, height
+}
+var index = require("./index.js")
+
+if (index.name === "Susan") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected: Susan, Received: "+index.name)
+}
+
+
+
+if (index.height === 70) {
+    console.log("The height is correct")
+} else {
+    console.log("Expected: 70, Received: "+index.height)
 }
 
